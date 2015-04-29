d0c6bf88c6aac6891554ff2c9b77717fd07c0fbf
wangdy0987 <wangdy0987@163.com>
2015-04-29 18:15:44 +0800

    add several model files.

---
+++ weixin/org/davewang/innovation/model/TextMessege.java
@@ -0,0 +1,20 @@
+package org.davewang.innovation.model;
+
+/** 
+ * 文本消息 
+ *  
+ * @author liufeng 
+ * @date 2013-05-19 
+ */  
+public class TextMessege extends BaseMessege {  
+    // 消息内容  
+    private String Content;  
+  
+    public String getContent() {  
+        return Content;  
+    }  
+  
+    public void setContent(String content) {  
+        Content = content;  
+    }  
+}  
